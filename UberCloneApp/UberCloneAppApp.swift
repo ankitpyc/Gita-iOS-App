3e42c1b433e95cbec9a0d09cd5cc1037c3db8669
misanki <ankitmishra471@gmail.com>
2023-12-17 20:24:32 +0530

    Initial Commit

---
+++ UberCloneApp/UberCloneAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberCloneAppApp.swift
+//  UberCloneApp
+//
+//  Created by Ankit Mishra on 17/12/23.
+//
+
+import SwiftUI
+
+@main
+struct UberCloneAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
